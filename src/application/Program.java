fc645724d906bbae201c299de8b5b0a4371999b8
victhor409 <victhor409@gmail.com>
2020-05-26 16:57:47 -0300

    findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -26,6 +26,13 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TESTE 3: seller findAll ====");
+		list= sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+		
 
 	}
 
