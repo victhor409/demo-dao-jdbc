38938017be7f665fef5d46f3ac9d7c79a817e1fc
victhor409 <victhor409@gmail.com>
2020-05-27 17:01:20 -0300

    Insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,7 +17,6 @@ public class Program {
 		
 		System.out.println("=== TESTE 1: seller findById ====");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
 		
 		System.out.println("\n=== TESTE 2: seller findByDepartment ====");
@@ -32,8 +31,10 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
-
-	}
-
+		System.out.println("\n=== TESTE 4: seller Insert ====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(),4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = "+ newSeller.getId());
+		}
+	
 }
